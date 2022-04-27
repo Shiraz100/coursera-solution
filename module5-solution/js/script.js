39e241292bd0c527569097eff89e3fc17edc6be6
Shiraz Ahmad <shiraz.ahmad@abasyn.edu.pk>
2022-04-27 08:55:18 +0500

    new

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -39,6 +39,7 @@ var showLoading = function (selector) {
 // Return substitute of '{{propName}}'
 // with propValue in given 'string'
 var insertProperty = function (string, propName, propValue) {
+  console.log(propValue);
   var propToReplace = "{{" + propName + "}}";
   string = string
     .replace(new RegExp(propToReplace, "g"), propValue);
